d303b1362e1216de6066c1c324cbe92515910477
Anderson Feitosa <andersonmfjr@gmail.com>
2022-04-29 17:15:47 -0300

    FEAT: changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     appId: '1:716510840470:web:e321afbb8674a9c73124b7',
     storageBucket: 'fill-source.appspot.com',
     apiKey: 'AIzaSyDC0Lqi4KLNLSBBKH60JmgfZAbR2-C5Vkc',
-    authDomain: 'fill-source.firebaseapp.com',
+    authDomain: 'wss://fill-source.firebaseapp.com',
     messagingSenderId: '716510840470',
   },
   production: true,
